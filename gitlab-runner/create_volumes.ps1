09aa699d07a7647a30a6f456f3224806fba29615
mikoto2000 <mikoto2000@gmail.com>
2019-07-29 00:56:46 +0900

    Added gitlab-runner.

---
+++ gitlab-runner/create_volumes.ps1
@@ -0,0 +1,5 @@
+docker volume create gitlab_etc
+docker volume create gitlab_opt
+docker volume create gitlab_log
+docker volume create gitlab-runner_config
+
---
+++ gitlab-runner/remove_volumes.ps1
@@ -0,0 +1,5 @@
+docker volume rm gitlab_etc
+docker volume rm gitlab_opt
+docker volume rm gitlab_log
+docker volume rm gitlab-runner_config
+
